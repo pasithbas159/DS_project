0e0bd649d45b49312ab3ad994af6e8b8603effb5
pasithbas159 <pasithbas@gmail.com>
2020-02-11 22:59:07 +0700

    second commit (4-7)

--- UCI_spyder.py
+++ UCI_spyder.py
@@ -39,4 +39,37 @@ plt.show()
 # =============================================================================
 sns.lmplot(x='G1', y='G2', data=mat, col='Dalc')
 sns.lmplot(x='G2', y='G3', data=mat, col='Dalc')
-#test
+# 4. train_test_split
+from sklearn.model_selection import train_test_split
+X = mat[['school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu',
+       'Mjob', 'Fjob', 'reason', 'guardian', 'traveltime', 'studytime',
+       'failures', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery',
+       'higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'Dalc',
+       'Walc', 'health', 'absences']]
+y = mat['G1']
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=101)
+# 5. pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+col_trans = make_column_transformer(
+        (OneHotEncoder(), ['school', 'sex', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu',
+        'reason', 'guardian', 'studytime',
+       'schoolsup', 'famsup', 'paid', 'activities', 'nursery',
+       'higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'Dalc'
+       , 'Walc', 'health']))
+col_trans
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+pipeline = make_pipeline(col_trans, model)
+pipeline
+# 6. modeling
+pipeline.fit(X_train, y_train)
+pipeline.score(X_train, y_train)
+y_pred = pipeline.predict(X_test).round(0)
+# 7. test accuracy
+from sklearn.metrics import confusion_matrix, classification_report
+print(confusion_matrix(y_test, y_pred))
+print('-'*100)
+print(classification_report(y_test, y_pred))
