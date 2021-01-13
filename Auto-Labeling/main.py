f235b3ad173501afee8f225a62cc364ebceaceb2
pasithbas159 <pasithbas@gmail.com>
2021-01-13 20:12:05 +0700

    Prototype Version (13/01/2021)

--- Auto-Labeling/main.py
+++ Auto-Labeling/main.py
@@ -7,6 +7,9 @@ import numpy as np
 import tensorflow as tf
 
 PATH = CONFIDENTIAL
+CLASS = 3
+TEST_SIZE = 0.1
+RND_STATE = 101
 
 class ManipulateData: 
     def __init__(self):
@@ -15,10 +18,10 @@ class ManipulateData:
         self.X = None
         self.y = None
         self.X_train = None
-        # self.X_val = None
+        self.X_val = None
         self.X_test = None
         self.y_train = None
-        # self.y_val = None
+        self.y_val = None
         self.y_test = None
         
     def import_data(self): 
@@ -29,10 +32,8 @@ class ManipulateData:
     def data_spliting(self, X, y): 
         self.X = X
         self.y = y
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.1, random_state=101)
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X, self.y, test_size=TEST_SIZE, random_state=RND_STATE)
         
-        
-    
     @property
     def getX_train(self): 
         self.X_train = self.X_train / 255.0
@@ -40,18 +41,18 @@ class ManipulateData:
     
     @property
     def gety_train(self): 
-        self.y_train = tf.keras.utils.to_categorical(self.y_train, 3)
+        self.y_train = tf.keras.utils.to_categorical(self.y_train, CLASS)
         return self.y_train
     
     @property
-    def getX_test(self): 
-        self.X_test = self.X_test / 255.0
-        return self.X_test
+    def getX_val(self): 
+        self.X_val = self.X_val / 255.0
+        return self.X_val
     
     @property
-    def gety_test(self): 
-        self.y_test = tf.keras.utils.to_categorical(self.y_test, 3)
-        return self.y_test
+    def gety_val(self): 
+        self.y_val = tf.keras.utils.to_categorical(self.y_val, CLASS)
+        return self.y_val
 
 
 mod = modeling.ModelingPart()
@@ -67,27 +68,25 @@ images_part.show_example_images(imgs=img_pixel, data=data)
 model = mod.CNN_3()
 X_train = manipulate_data.getX_train
 y_train = manipulate_data.gety_train
-X_test = manipulate_data.getX_test
-y_test = manipulate_data.gety_test
+X_val = manipulate_data.getX_val
+y_val = manipulate_data.gety_val
 
 history = mod.model_train(model=model, 
                           X_train=np.asarray(X_train).astype('float32'), 
                           y_train=np.asarray(y_train).astype('float32'), 
-                          X_test=np.asarray(X_test).astype('float32'), 
-                          y_test=np.asarray(y_test).astype('float32'))
+                          X_val=np.asarray(X_val).astype('float32'), 
+                          y_val=np.asarray(y_val).astype('float32'))
 
 mod.model_acc_plot(history)
 mod.model_loss_plot(history)
 
 pred = []
-pred = mod.prediction(X_test, y_test)
+pred = mod.prediction(X_val, y_val)
 Y_pred = pred[0]
 Y_true = pred[1]
 print(classification_report(Y_true, Y_pred))
 
-images_part.plot_results(X_test, Y_pred, Y_true)
-
-# print(history)
+images_part.plot_results(X_val, Y_pred, Y_true)
 
 # =============================================================================
 # if __name__ == '__main__': 
--- Auto-Labeling/modeling.py
+++ Auto-Labeling/modeling.py
@@ -5,6 +5,7 @@ import numpy as np
 
 keras = tf.keras
 INITIAL_LR = 1e-4
+CLASS = 3
 
 class ModelingPart(): 
     def __init__(self):
@@ -15,34 +16,35 @@ class ModelingPart():
         
     def CNN_3(self): 
         self.model = keras.Sequential([
-            keras.layers.Conv2D(32, (3,3), activation='relu', input_shape = (50,50,3)), 
+            keras.layers.Conv2D(16, (3,3), activation='relu', input_shape = (50,50,3)), 
+            keras.layers.MaxPooling2D(2,2), 
+            keras.layers.Conv2D(32, (3,3), activation='relu'), 
             keras.layers.MaxPooling2D(2,2), 
             keras.layers.Conv2D(64, (3,3), activation='relu'), 
-            keras.layers.MaxPooling2D(2,2), 
-            # keras.layers.Conv2D(128, (3,3), activation='relu'), 
-            # keras.layers.MaxPooling2D(2,2),
+            keras.layers.MaxPooling2D(2,2),
             keras.layers.Flatten(), 
-            keras.layers.Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l2(0.001)), 
-            keras.layers.Dropout(.2),
-            keras.layers.Dense(3, activation='softmax')
+            keras.layers.Dense(256, activation='relu', kernel_regularizer=keras.regularizers.l2(0.01)), 
+            # keras.layers.BatchNormalization(axis=1, momentum=0.99, epsilon=0.001),
+            keras.layers.Dropout(.3),
+            keras.layers.Dense(CLASS, activation='softmax')
             ])
         
         # print(self.model.summary())
         
         return self.model
-    
-    def model_train(self, model, X_train, y_train, X_test, y_test): 
+
+    def model_train(self, model, X_train, y_train, X_val, y_val): 
         self.lr_schedule = keras.optimizers.schedules.ExponentialDecay(
                         INITIAL_LR,
-                        decay_steps=500,
-                        decay_rate=0.91,
+                        decay_steps=1000,
+                        decay_rate=0.9,
                         staircase=False)
         
         model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.lr_schedule), loss='categorical_crossentropy', metrics=['acc'])
         checkpoint = keras.callbacks.ModelCheckpoint('model-{epoch:03d}-{acc:03f}-{val_acc:03f}.h5', save_best_only=True, monitor='val_acc', mode='max')
-        self.history = model.fit(X_train, y_train, epochs=200, validation_data=(X_test, y_test), callbacks=[checkpoint])
+        self.history = model.fit(X_train, y_train, epochs=250, validation_data=(X_val, y_val), callbacks=[checkpoint])
         
-        model.evaluate(X_test, y_test)
+        model.evaluate(X_val, y_val)
         
         return self.history
     
@@ -78,13 +80,23 @@ class ModelingPart():
 
         plt.show()
         
-    def prediction(self, X_test, y_test): 
-        pred = self.model.predict(X_test)
+    def prediction(self, X_val, y_val): 
+        # after run
+        # self.model = keras.models.load_model("Model/model-140-0.707832-0.689840.h5")
+        pred = self.model.predict(X_val)
         self.Y_pred_classes = np.argmax(pred, axis=1) 
-        self.Y_true = np.argmax(y_test, axis=1)
+        self.Y_true = np.argmax(y_val, axis=1)
         
         print(self.Y_pred_classes)
         print(self.Y_true)
         
         return self.Y_pred_classes, self.Y_true
+    
+    def new_data_prediction(self, X_test): 
+        # after run
+        self.model = keras.models.load_model("Model/model-140-0.707832-0.689840.h5")
+        pred = self.model.predict(X_test)
+        self.Y_pred_classes = np.argmax(pred, axis=1) 
+        
+        return self.Y_pred_classes
     
